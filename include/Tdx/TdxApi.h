925061adcfb42b29b8040116eebb56c8ba627967
伍侃 <wu-kan@163.com>
2016-11-04 09:39:35 +0800

    库更新

--- include/Tdx/TdxApi.h
+++ include/Tdx/TdxApi.h
@@ -51,7 +51,7 @@ public:
 	virtual void Version() = 0;
 
 	virtual int GetTableHeader(void* client, int request, FieldInfo_STRUCT*** pppResults) = 0;
-	virtual GDLB_STRUCT** GetGDLB(void* client) = 0;
+	virtual bool IsLogined(void* client) = 0;
 
 	virtual int SendRequest(IN RequestResponse_STRUCT* pRequest) = 0;
 
